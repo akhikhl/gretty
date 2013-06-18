b887c53a461ed753f652bb2a961f0d2b33f12027
Andrey Khikhlovskiy <ahi@startext.de>
2013-06-18 15:21:53 +0200

    added serialVersionUID to servlet

--- examples/helloGretty/src/main/java/hellogretty/ExampleServlet.java
+++ examples/helloGretty/src/main/java/hellogretty/ExampleServlet.java
@@ -3,16 +3,21 @@ package hellogretty;
 import java.io.IOException;
 import java.io.PrintWriter;
 
-import javax.servlet.*;
-import javax.servlet.http.*;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 
 import org.apache.commons.lang3.StringUtils;
 
 public class ExampleServlet extends HttpServlet {
 
-	protected void doGet(
-    HttpServletRequest request,
-    HttpServletResponse response) throws ServletException, IOException {
+  private static final long serialVersionUID = -6506276378398106663L;
+
+  @Override
+  protected void doGet(
+      HttpServletRequest request,
+      HttpServletResponse response) throws ServletException, IOException {
 
     try {
       PrintWriter out = response.getWriter();
@@ -23,8 +28,7 @@ public class ExampleServlet extends HttpServlet {
       } finally {
         out.close();
       }
-    }
-    catch (Exception ex) {
+    } catch (Exception ex) {
       throw new ServletException(ex);
     }
   }
