049d42bf5bb12a3fbf2058600eeb57becd7d924e
Andrey Hihlovskiy <akhikhl@gmail.com>
2013-06-23 17:27:40 +0200

    updated helloGretty example: now using apache-commons-configuration to demonstrate library integration.

--- examples/helloGretty/src/main/java/hellogretty/ExampleServlet.java
+++ examples/helloGretty/src/main/java/hellogretty/ExampleServlet.java
@@ -8,21 +8,21 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-import org.apache.commons.lang3.StringUtils;
+import org.apache.commons.configuration.PropertiesConfiguration;
 
 public class ExampleServlet extends HttpServlet {
 
   private static final long serialVersionUID = -6506276378398106663L;
 
   @Override
-  protected void doGet(
-      HttpServletRequest request,
-      HttpServletResponse response) throws ServletException, IOException {
-
+  protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
     try {
+      PropertiesConfiguration config = new PropertiesConfiguration();
+      config.load(this.getClass().getClassLoader().getResource("hellogretty/messages.properties"));
+      String greeting = config.getString("greeting");
       PrintWriter out = response.getWriter();
       try {
-        out.println("<html><body><h1>Hello, Gretty! It's fine weather today" + StringUtils.repeat('.', 3) + "</h1></body></html>");
+        out.println("<html><body><h1>" + greeting + "</h1></body></html>");
         out.println();
         out.flush();
       } finally {
