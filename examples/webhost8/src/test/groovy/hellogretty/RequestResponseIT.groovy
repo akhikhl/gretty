b0ad69e6e8e93366b65612e28bbe470352d93d1b
Andrey Hihlovskiy <ahi@startext.de>
2014-03-06 09:24:11 +0100

    implemented integration test for META-INF/resources loading

--- examples/webhost8/src/test/groovy/hellogretty/RequestResponseIT.groovy
+++ examples/webhost8/src/test/groovy/hellogretty/RequestResponseIT.groovy
@@ -4,10 +4,17 @@ import geb.spock.GebReportingSpec
 
 class RequestResponseIT extends GebReportingSpec {
 
-  def 'should get expected response from the server'() {
+  def 'should get response from servlet declared in META-INF/web-fragment.xml'() {
   when:
-    go 'http://localhost:8080/webhost8'
+    go 'http://localhost:8080/webhost8/exampleservlet'
   then:
     $('h1').text() == /Hello, this is webfragment.ExampleServlet/
+  }
+
+  def 'should see static files in META-INF/resources'() {
+  when:
+    go 'http://localhost:8080/webhost8/page1.htm'
+  then:
+    $('h1').text() == /page loaded from webfragment/
   }
 }
