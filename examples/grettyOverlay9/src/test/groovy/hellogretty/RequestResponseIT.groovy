49e340e642e62ed366a0eb7662f48c5b296b55d0
Andrey Hihlovskiy <akhikhl@gmail.com>
2014-04-02 22:04:40 +0200

    fixed integration tests

--- examples/grettyOverlay9/src/test/groovy/hellogretty/RequestResponseIT.groovy
+++ examples/grettyOverlay9/src/test/groovy/hellogretty/RequestResponseIT.groovy
@@ -4,10 +4,17 @@ import geb.spock.GebReportingSpec
 
 class RequestResponseIT extends GebReportingSpec {
 
-  def 'should get expected response from the server'() {
+  def 'should get expected response from servlet'() {
   when:
-    go 'http://localhost:8080/grettyOverlay9'
+    go 'http://localhost:8080/grettyOverlay9/hello'
   then:
     $('h1').text() == /Hello, Gretty! This is a message from overlayed web-application!/
+  }
+
+  def 'should get expected static page'() {
+  when:
+    go 'http://localhost:8080/grettyOverlay9/test.html'
+  then:
+    $('div').text() == 'TODO write content'
   }
 }
--- examples/helloGretty9/src/test/groovy/hellogretty/RequestResponseIT.groovy
+++ examples/helloGretty9/src/test/groovy/hellogretty/RequestResponseIT.groovy
@@ -4,10 +4,17 @@ import geb.spock.GebReportingSpec
 
 class RequestResponseIT extends GebReportingSpec {
 
-  def 'should get expected response from the server'() {
+  def 'should get expected response from servlet'() {
   when:
-    go 'http://localhost:8080/helloGretty9'
+    go 'http://localhost:8080/helloGretty9/hello'
   then:
     $('h1').text() == /Hello, Gretty! It's fine weather today, isn't it?/
+  }
+
+  def 'should get expected static page'() {
+  when:
+    go 'http://localhost:8080/helloGretty9/test.html'
+  then:
+    $('div').text() == 'TODO write content'
   }
 }
