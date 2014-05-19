5cac43339e3b50914fa5b175a4ea2c387510969a
Andrey Hihlovskiy <akhikhl@gmail.com>
2014-05-19 20:37:45 +0200

    added classes GrettyStartTask and GrettyServiceTask for backward compatibility

---
+++ libs/gretty-plugin-commons/src/main/groovy/org/akhikhl/gretty/GrettyServiceTask.groovy
@@ -0,0 +1,23 @@
+/*
+ * gretty
+ *
+ * Copyright 2013  Andrey Hihlovskiy.
+ *
+ * See the file "license.txt" for copying and usage permission.
+ */
+package org.akhikhl.gretty
+
+/**
+ * This class is deprecated, please use JettyServiceTask instead.
+ *
+ * @author akhikhl
+ */
+@Deprecated
+class GrettyServiceTask extends JettyServiceTask {
+
+  String command
+
+  String getCommand() {
+    command
+  }
+}
---
+++ libs/gretty-plugin-commons/src/main/groovy/org/akhikhl/gretty/GrettyStartTask.groovy
@@ -0,0 +1,18 @@
+/*
+ * gretty
+ *
+ * Copyright 2013  Andrey Hihlovskiy.
+ *
+ * See the file "license.txt" for copying and usage permission.
+ */
+package org.akhikhl.gretty
+
+/**
+ * This class is deprecated, please use JettyStartTask instead.
+ *
+ * @author akhikhl
+ */
+@Deprecated
+class GrettyStartTask extends JettyStartTask {
+
+}
