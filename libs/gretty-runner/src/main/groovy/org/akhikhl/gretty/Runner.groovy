f5c44cc7441a5c90f5e64c7f351c1060f28be34f
Andrey Hihlovskiy <akhikhl@gmail.com>
2014-06-18 21:13:22 +0200

    merged RunnerBase and Runner classes

--- libs/gretty-runner/src/main/groovy/org/akhikhl/gretty/Runner.groovy
+++ libs/gretty-runner/src/main/groovy/org/akhikhl/gretty/Runner.groovy
@@ -13,7 +13,10 @@ import groovy.json.JsonSlurper
  *
  * @author akhikhl
  */
-final class Runner extends RunnerBase {
+final class Runner {
+
+  protected final Map params
+  protected boolean paramsLoaded = false
 
   static void main(String[] args) {
     assert args.length != 0
@@ -22,12 +25,43 @@ final class Runner extends RunnerBase {
   }
 
   private Runner(Map params) {
-    super(params)
+    this.params = params
   }
 
-  @Override
-  protected ServerManager createServerManager() {
+  private void run() {
     def ServerManagerFactory = Class.forName(params.serverManagerFactory, true, this.getClass().classLoader)
-    ServerManagerFactory.createServerManager()
-  }
+    ServerManager serverManager = ServerManagerFactory.createServerManager()
+    try {
+      ServerSocket socket = new ServerSocket(params.servicePort, 1, InetAddress.getByName('127.0.0.1'))
+      try {
+        ServiceProtocol.send(params.statusPort, 'init')
+        while(true) {
+          def data = ServiceProtocol.readMessageFromServerSocket(socket)
+          if(!paramsLoaded) {
+            params << new JsonSlurper().parseText(data)
+            paramsLoaded = true
+            serverManager.setParams(params)
+            serverManager.startServer()
+            ServiceProtocol.send(params.statusPort, 'started')
+            // Note that server is already in listening state.
+            // If client sends a command immediately after 'started' signal,
+            // the command is queued, so that socket.accept gets it anyway.
+            continue
+          }
+          if(data == 'stop') {
+            serverManager.stopServer()
+            break
+          }
+          else if(data == 'restart') {
+            serverManager.stopServer()
+            serverManager.startServer()
+          }
+        }
+      } finally {
+        socket.close()
+      }
+    } catch(Exception e) {
+      throw new RuntimeException(e)
+    }
+  }  
 }
