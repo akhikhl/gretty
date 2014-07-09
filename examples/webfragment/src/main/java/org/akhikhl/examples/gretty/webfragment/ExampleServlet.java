ff3acd492518c7478b1b24f6f44dfd6c58b5e2bf
Andrey Hihlovskiy <akhikhl@gmail.com>
2014-07-09 19:05:48 +0200

    reworked web-fragment examples

---
+++ examples/webfragment/src/main/java/org/akhikhl/examples/gretty/webfragment/ExampleServlet.java
@@ -0,0 +1,44 @@
+package org.akhikhl.examples.gretty.webfragment;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.ServletException;
+import org.apache.velocity.Template;
+import org.apache.velocity.VelocityContext;
+import org.apache.velocity.app.VelocityEngine;
+import org.apache.velocity.runtime.RuntimeConstants;
+import org.apache.velocity.runtime.resource.loader.ClasspathResourceLoader;
+
+public class ExampleServlet extends HttpServlet {
+
+  private static final long serialVersionUID = -6506276378398106663L;
+
+  private VelocityEngine ve = new VelocityEngine();
+
+  @Override
+  protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+    Template template = ve.getTemplate("/org/akhikhl/examples/gretty/webfragment/templates/servletpage.html", "UTF-8");
+    VelocityContext context = new VelocityContext();
+    context.put("contextPath", request.getContextPath());
+    context.put("today", new java.util.Date());
+    PrintWriter out = response.getWriter();
+    try {
+      template.merge(context, out);
+      out.flush();
+    } finally {
+      out.close();
+    }
+  }
+
+  @Override
+  public void init() throws ServletException {
+    super.init();
+    ve.setProperty(RuntimeConstants.RESOURCE_LOADER, "classpath");
+    ve.setProperty("classpath.resource.loader.class", ClasspathResourceLoader.class.getName());
+    ve.init();
+  }
+}
+
---
+++ examples/webhost/README.md
@@ -0,0 +1,17 @@
+# webhost
+
+Example showing use of web-fragments.
+
+## Usage
+
+- Run from command-line:
+
+```
+gradle appRun
+```
+
+- Open in browser:
+
+```
+http://localhost:8080/webhost/
+```
