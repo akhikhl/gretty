7a8e1d5fff196f798d2cbbb42c43526414b814dc
Andrey Hihlovskiy <akhikhl@gmail.com>
2014-07-11 16:04:25 +0200

    implemented automatic reordering of FarmBeforeIntegrationTest and FarmAfterIntegrationTest tasks across multi-project and multi-farm environment

--- libs/gretty/src/main/groovy/org/akhikhl/gretty/FarmBeforeIntegrationTestTask.groovy
+++ libs/gretty/src/main/groovy/org/akhikhl/gretty/FarmBeforeIntegrationTestTask.groovy
@@ -7,6 +7,7 @@
  */
 package org.akhikhl.gretty
 
+import org.gradle.api.GradleException
 import org.gradle.api.Project
 import org.gradle.process.JavaForkOptions
 import org.slf4j.Logger
@@ -25,6 +26,28 @@ class FarmBeforeIntegrationTestTask extends FarmStartTask {
 
   FarmBeforeIntegrationTestTask() {
     def thisTask = this
+    mustRunAfter {
+      List projects = project.rootProject.allprojects as List
+      def result = []
+      int thisProjectIndex = projects.indexOf(project)
+      if(thisProjectIndex > 0)
+        result.addAll projects[0..thisProjectIndex - 1].findAll { it.extensions.findByName('farms') }.collectMany { proj ->
+          proj.extensions.farms.farmsMap.keySet().collect { proj.tasks['farmAfterIntegrationTest' + it] }
+        }
+      def farms = project.extensions.farms.farmsMap.keySet() as List
+      def thisFarmIndex = farms.indexOf(farmName)
+      if(thisFarmIndex > 0)
+        result.addAll farms[0..thisFarmIndex - 1].collect { project.tasks['farmAfterIntegrationTest' + it] }
+      //def collectIntegrationTaskKeys = { it.getWebAppProjects().findResults { it.tasks.findByName(thisTask.integrationTestTask) ? it.path + ':' + thisTask.integrationTestTask : null } }
+      result.each { fait ->
+        if(thisTask.integrationTestTask == fait.integrationTestTask) {
+          def intersection = thisTask.getWebAppProjects().intersect(fait.getWebAppProjects())
+          if(intersection)
+            throw new GradleException("Could not properly order farm tasks: $thisTask intersects with $fait over projects ${intersection.collect { it.path } }")
+        }
+      }
+      result
+    }
     doFirst {
       getWebAppProjects().each { proj ->
         proj.tasks.each { t ->
@@ -123,7 +146,7 @@ class FarmBeforeIntegrationTestTask extends FarmStartTask {
       task.systemProperty 'gretty.preferredProtocol', preferredProtocol
     if(preferredBaseURI)
       task.systemProperty 'gretty.preferredBaseURI', preferredBaseURI
-      
+
     task.systemProperty 'gretty.farm', (farmName ?: 'default')
   }
 }
