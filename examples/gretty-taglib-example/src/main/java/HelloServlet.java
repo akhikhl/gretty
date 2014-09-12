6d4e1d4fdbc9334f314d43c6b786af03bb569850
Andrey Hihlovskiy <akhikhl@gmail.com>
2014-09-12 18:45:36 +0200

    gretty-taglib-example: WEB-INF/libs -> WEB-INF/lib

--- examples/gretty-taglib-example/src/main/java/HelloServlet.java
+++ examples/gretty-taglib-example/src/main/java/HelloServlet.java
@@ -8,12 +8,12 @@ import java.util.Set;
 public class HelloServlet extends HttpServlet {
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-        final Set<String> libs = getServletContext().getResourcePaths("/WEB-INF/libs");
-        if(libs.isEmpty()) {
+        final Set<String> libs = getServletContext().getResourcePaths("/WEB-INF/lib");
+        if(libs == null || libs.isEmpty()) {
             // Following output will be produced by Gretty
-            resp.getOutputStream().print("<p>/WEB-INF/libs are empty</p>");
+            resp.getOutputStream().print("<p>/WEB-INF/lib are empty</p>");
         } else {
-            resp.getOutputStream().print("<p>/WEB-INF/libs are not empty</p>");
+            resp.getOutputStream().print("<p>/WEB-INF/lib are not empty</p>");
             resp.getOutputStream().print("<ul>");
             // Following output will be produced by maven-jetty-plugin:
             for (String lib : libs) {
