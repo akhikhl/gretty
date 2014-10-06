dd4d61fc2e4a13e7e4425a3e86d52acd6047f8d5
Andrey Hihlovskiy <akhikhl@gmail.com>
2014-10-06 12:48:59 +0200

    added useFileMappedBuffer=false to jetty configurations, so that static files are not blocked when running Gretty tasks

--- libs/gretty-runner-jetty8/src/main/groovy/org/akhikhl/gretty/JettyConfigurerImpl.groovy
+++ libs/gretty-runner-jetty8/src/main/groovy/org/akhikhl/gretty/JettyConfigurerImpl.groovy
@@ -210,6 +210,7 @@ class JettyConfigurerImpl implements JettyConfigurer {
     context.addSystemClass('org.codehaus.groovy.')
     context.addSystemClass('groovy.lang.')
     context.setExtraClasspath(webappClassPath.collect { it.endsWith('.jar') ? it : (it.endsWith('/') ? it : it + '/') }.findAll { !isServletApi(it) }.join(';'))
+    context.setOverrideDescriptor('/org/akhikhl/gretty/override-web.xml')
     context.addEventListener(new ContextDetachingSCL())
     context.addFilter(LoggerContextFilter.class, '/*', EnumSet.of(DispatcherType.REQUEST))
     return context
