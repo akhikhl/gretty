c8ac5a5491f7cae083af5b4b82daaa50b9821b1c
Andrey Hihlovskiy <akhikhl@gmail.com>
2015-02-09 18:48:04 +0100

    implemented automatic injection of slf4j-nop, when slf4j-api is present, but no StaticLoggerBinder implementation is present

--- libs/gretty/src/main/groovy/org/akhikhl/gretty/GrettyPlugin.groovy
+++ libs/gretty/src/main/groovy/org/akhikhl/gretty/GrettyPlugin.groovy
@@ -11,6 +11,7 @@ import org.gradle.api.GradleException
 import org.gradle.api.Plugin
 import org.gradle.api.Project
 import groovy.util.XmlSlurper
+import org.gradle.api.artifacts.ResolvedArtifact
 import org.slf4j.Logger
 import org.slf4j.LoggerFactory
 /**
@@ -92,6 +93,7 @@ class GrettyPlugin implements Plugin<Project> {
     String grettyVersion = Externalized.getString('grettyVersion')
     String springBootVersion = project.gretty.springBootVersion ?: (project.hasProperty('springBootVersion') ? project.springBootVersion : Externalized.getString('springBootVersion'))
     String springLoadedVersion = project.gretty.springLoadedVersion ?: (project.hasProperty('springLoadedVersion') ? project.springLoadedVersion : Externalized.getString('springLoadedVersion'))
+    String slf4jVersion = Externalized.getString('slf4jVersion')
 
     project.dependencies {
       grettyStarter "org.akhikhl.gretty:gretty-starter:$grettyVersion"
@@ -164,6 +166,13 @@ class GrettyPlugin implements Plugin<Project> {
         }
       }
       alteredDependencies
+    }
+
+    def artifacts = project.configurations.runtime.copyRecursive().resolvedConfiguration.resolvedArtifacts
+    if(artifacts.find { it.name == 'slf4j-api' } && !artifacts.find { it.name in ['slf4j-nop', 'slf4j-simple', 'slf4j-log4j12', 'slf4j-jdk14', 'logback-classic'] }) {
+      project.dependencies {
+        compile "org.slf4j:slf4j-nop:$slf4jVersion"
+      }
     }
   }
 
