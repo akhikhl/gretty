2e632e3580b151982677ec8d1c3dcf95857ce5a1
Andrey Hihlovskiy <akhikhl@gmail.com>
2015-04-11 09:33:28 +0200

    cleanup in examples

--- examples/springBootWebSocket/src/main/java/org/akhikhl/examples/gretty/springbootwebsocket/GreetingController.java
+++ examples/springBootWebSocket/src/main/java/org/akhikhl/examples/gretty/springbootwebsocket/GreetingController.java
@@ -1,3 +1,11 @@
+/*
+ * Gretty
+ *
+ * Copyright (C) 2013-2015 Andrey Hihlovskiy and contributors.
+ *
+ * See the file "LICENSE" for copying and usage permission.
+ * See the file "CONTRIBUTORS" for complete list of contributors.
+ */
 package org.akhikhl.examples.gretty.springbootwebsocket;
 
 import org.springframework.messaging.handler.annotation.MessageMapping;
