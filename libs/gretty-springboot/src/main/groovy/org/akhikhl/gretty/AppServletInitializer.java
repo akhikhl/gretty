e199901c063323aada4f75519ee6402acd8cdd7b
henarons <henrik.brautaset.aronsen@finn.no>
2018-05-14 11:59:16 +0200

    Spring Boot 2

--- libs/gretty-springboot/src/main/groovy/org/akhikhl/gretty/AppServletInitializer.java
+++ libs/gretty-springboot/src/main/groovy/org/akhikhl/gretty/AppServletInitializer.java
@@ -1,7 +1,7 @@
 package org.akhikhl.gretty;
 
 import org.springframework.boot.builder.SpringApplicationBuilder;
-import org.springframework.boot.web.support.SpringBootServletInitializer;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 public class AppServletInitializer extends SpringBootServletInitializer {
 
